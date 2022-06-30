69cf22feb79ea1d3748ad835502ec0199dc77547
UDAYAPRAKASH KOTHANDAPANI <udaya.prakash@tcs.com>
2022-06-30 17:38:35 +0530

    add greeting controller

---
+++ src/main/java/apps/controller/HelloController.java
@@ -0,0 +1,12 @@
+package apps.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+   @GetMapping("/greeting")
+   public String greeting() {
+       return "Hello World!";
+   } 
+}
