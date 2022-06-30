90cf01ad9fed134df34efac7bc793aa7fd035581
UDAYAPRAKASH KOTHANDAPANI <udaya.prakash@tcs.com>
2022-06-30 17:19:55 +0530

    init commit

---
+++ src/main/java/apps/RedisCacheApiApplication.java
@@ -0,0 +1,13 @@
+package apps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCacheApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisCacheApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/apps/RedisCacheApiApplicationTests.java
@@ -0,0 +1,13 @@
+package apps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCacheApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
